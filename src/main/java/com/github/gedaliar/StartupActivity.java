9b67c7379a7e2e0d04ec0f2b0d9935894a404339
grabinow2 <grabinow2@student.touro.edu>
2021-03-08 17:38:29 -0500

    Added Basic Message Dialog on Startup.

---
+++ src/main/java/com/github/gedaliar/StartupActivity.java
@@ -0,0 +1,15 @@
+package com.github.gedaliar;
+
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.ui.Messages;
+import org.jetbrains.annotations.NotNull;
+
+public class StartupActivity implements com.intellij.openapi.startup.StartupActivity {
+
+    @Override
+    public void runActivity(@NotNull Project project) {
+        Messages.showMessageDialog(project, "Hello World!", "Quote of the Day", Messages.getInformationIcon());
+    }
+
+
+}
